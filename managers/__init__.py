c94cf7d70e540fc1f935a20720c003e5cbe2422f
Arshia Bourbour <ar2003br@gmail.com>
2026-09-16 18:30:34 +0330

    export the asset manager from the managers package

--- managers/__init__.py
+++ managers/__init__.py
@@ -1,12 +1,15 @@
 """Systems that own external resources: assets, audio and save data."""
 
+from managers.asset_manager import AssetManager, shared_asset_manager
 from managers.audio_manager import AudioManager
 from managers.resource_manager import ResourceManager
 from managers.save_manager import SaveData, SaveManager
 
 __all__ = [
+    "AssetManager",
     "AudioManager",
     "ResourceManager",
     "SaveData",
     "SaveManager",
+    "shared_asset_manager",
 ]
